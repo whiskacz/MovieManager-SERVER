465ea4164002043b0d9cbc17a77c46bff860b22e
whiskacz <pawelblasiak1985@gmail.com>
2023-12-12 22:15:51 +0100

    favorites movies list done

--- routes/routes.js
+++ routes/routes.js
@@ -23,61 +23,62 @@ app.post("/create", async (request, response) => {
     }
   });
 
-  app.post("/login", async (request, response) => {
-    const { user, password } = request.body;
-    
+app.post("/login", async (request, response) => {
+const { user, password } = request.body;
+
+try {
+    // Wyszukanie użytkownika po nazwie użytkownika (lub innym unikalnym identyfikatorze)
+    const foundUser = await userModel.findOne({ user });
+
+    if (foundUser) {
+        // Porównanie hasła wprowadzonego przez użytkownika z zahashowanym hasłem z bazy danych
+        const isPasswordValid = await bcrypt.compare(password, foundUser.password);
+
+        if (isPasswordValid) {
+            // Użytkownik został uwierzytelniony poprawnie
+            response.status(200).json({ message: "Authentication successful" });
+        } else {
+            // Nieprawidłowe hasło
+            response.status(401).json({ message: "Invalid password" });
+        }
+    } else {
+        // Użytkownik nie istnieje
+        response.status(404).json({ message: "User not found" });
+    }
+} catch (error) {
+    response.status(500).json({ message: "Server error" });
+}
+
+});
+
+app.post("/moviesEdit", async (request, response) => {
+    const { user, action, movieId } = request.body;
+
     try {
-        // Wyszukanie użytkownika po nazwie użytkownika (lub innym unikalnym identyfikatorze)
+       
         const foundUser = await userModel.findOne({ user });
 
         if (foundUser) {
-            // Porównanie hasła wprowadzonego przez użytkownika z zahashowanym hasłem z bazy danych
-            const isPasswordValid = await bcrypt.compare(password, foundUser.password);
-
-            if (isPasswordValid) {
-                // Użytkownik został uwierzytelniony poprawnie
-                response.status(200).json({ message: "Authentication successful" });
+            if (action === "add") {
+                // Dodaj identyfikator filmu do listy filmów użytkownika
+                foundUser.movies.push(movieId);
+                response.status(200).json({ message: "Movie added to user's collection successfully" });
+            } else if (action === "remove") {
+                // Usuń identyfikator filmu z listy filmów użytkownika
+                foundUser.movies = foundUser.movies.filter(id => id !== movieId);
+                response.status(200).json({ message: "Movie removed from user's collection successfully" });
             } else {
-                // Nieprawidłowe hasło
-                response.status(401).json({ message: "Invalid password" });
+                response.status(400).json({ message: "Invalid action" });
             }
+
+            // Zapisz zmiany w bazie danych
+            await foundUser.save();
         } else {
-            // Użytkownik nie istnieje
             response.status(404).json({ message: "User not found" });
         }
     } catch (error) {
         response.status(500).json({ message: "Server error" });
     }
-
-    app.post("/moviesEdit", async (request, response) => {
-        const { userId, action, movieId } = request.body;
-    
-        try {
-            // Znajdź użytkownika po ID
-            const foundUser = await userModel.findById(userId);
-    
-            if (foundUser) {
-                if (action === "add") {
-                    // Dodaj identyfikator filmu do listy filmów użytkownika
-                    foundUser.movies.push(movieId);
-                    response.status(200).json({ message: "Movie added to user's collection successfully" });
-                } else if (action === "remove") {
-                    // Usuń identyfikator filmu z listy filmów użytkownika
-                    foundUser.movies = foundUser.movies.filter(id => id !== movieId);
-                    response.status(200).json({ message: "Movie removed from user's collection successfully" });
-                } else {
-                    response.status(400).json({ message: "Invalid action" });
-                }
-    
-                // Zapisz zmiany w bazie danych
-                await foundUser.save();
-            } else {
-                response.status(404).json({ message: "User not found" });
-            }
-        } catch (error) {
-            response.status(500).json({ message: "Server error" });
-        }
-    });
 });
 
 module.exports = app
